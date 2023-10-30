78daeb2f98ad95df3d38d3d01bf43dbc3f56e99c
MSI\aasharib <aasharib@onescreensolutions.com>
2023-10-30 20:09:08 +0500

    feat: implemented minimum functionality for app bubble

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,63 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { StyleSheet, View, Text, ToastAndroid, DeviceEventEmitter, Button } from 'react-native';
+import { multiply, add, showFloatingBubble, hideFloatingBubble, requestPermission, checkPermission, initialize, reopenApp, isBubbleOpen } from 'react-native-app-bubble';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-app-bubble';
+const showToast = (text:any) => ToastAndroid.show(text, 1000)
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
+  const [addResult, setAddResult] = React.useState<number | undefined>();
+  const onAdd = () => showFloatingBubble().then(() => showToast("Add Floating Button"))
+  const onHide = () => hideFloatingBubble().then(() => showToast("Manually Removed Bubble")).catch(() => showToast("Failed to remove"))
+  const onRequestPermission = () => requestPermission().then(() => showToast("Permission received")).catch(() => showToast("Failed to get permission"))
+  const onCheckPermissoin = () => checkPermission().then((value:any) => showToast(`Permission: ${value ? 'Yes' : 'No'}`)).catch(() => showToast("Failed to check"))
+  const onInit = () => initialize().then(() => showToast("Init")).catch(() => showToast("Failed init"));
+  const onBubblePress = () =>  reopenApp();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const showOnlyOneBubble = async () => {
+    try {
+      const _isBubbleOpen = await isBubbleOpen();
+
+      if (!_isBubbleOpen) {
+        await showFloatingBubble();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  useEffect(() => {
+    const subscriptionPress = DeviceEventEmitter.addListener("floating-bubble-press", function (e) {
+      // showToast("Press Bubble")
+      onBubblePress();
+      hideFloatingBubble();
+      // reopenApp().then(()=>{hideFloatingBubble()});
+    });
+    const subscriptionRemove = DeviceEventEmitter.addListener("floating-bubble-remove", function (e) {
+      showToast("Remove Bubble")
+    });
+    return () => {
+      subscriptionPress.remove();
+      subscriptionRemove.remove();
+    }
+  }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View>
+      <View style={{ padding: 30 }}>
+        <Text>Check Permission</Text>
+        <Button style={styles.button} title="Check" onPress={onCheckPermissoin} />
+        <Text>Get Permission</Text>
+        <Button style={styles.button} title="Get Permission" onPress={onRequestPermission} />
+        <Text>Initialize Bubble Manage</Text>
+        <Button style={styles.button} title="Initialize" onPress={onInit} />
+        <Text>Add the bubble</Text>
+        <Button style={styles.button} title="Add Bubble" onPress={showOnlyOneBubble} />
+        <Text>Remove the bubble</Text>
+        <Button style={styles.button} title="Hide Bubble" onPress={onHide} />
+      </View>
     </View>
   );
 }
@@ -27,5 +72,8 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  button: {
+    margin: 30
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,34 @@ const AppBubble = NativeModules.AppBubble
       }
     );
 
+
+    
+// const { RNFloatingBubble } = NativeModules;
+
+export const reopenApp = () => AppBubble.reopenApp();
+export const showFloatingBubble = (x = 50, y = 100) => AppBubble.showFloatingBubble(x, y);
+export const hideFloatingBubble = () => AppBubble.hideFloatingBubble();
+export const checkPermission = () => AppBubble.checkPermission();
+export const requestPermission = () => AppBubble.requestPermission();
+export const initialize = () => AppBubble.initialize();
+export const isBubbleOpen = () => AppBubble.isBubbleOpen();
+  
 export function multiply(a: number, b: number): Promise<number> {
   return AppBubble.multiply(a, b);
 }
+
+export function add(a: number, b: number): Promise<number> {
+  return AppBubble.add(a, b);
+}
+
+export default {
+  showFloatingBubble,
+  hideFloatingBubble,
+  requestPermission,
+  checkPermission,
+  initialize,
+  reopenApp,
+  isBubbleOpen,
+  add,
+  multiply
+};
